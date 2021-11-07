4685fa7fec76039fa27cc5e461ac1d59c41309de
arju9801 <arju9801@gmail.com>
2021-11-08 03:04:52 +0530

    some cahnges

--- fetch_products.py
+++ fetch_products.py
@@ -14,7 +14,7 @@ import re
 
 # In[7]:
 
-products = ["short","pant","bellie","shirt","tshirt","lower","jean","clock","decal","sticker","decor","pant","shoe","sneaker","sandal","sunscreen","bedsheet","pillow","cushion","cup","perfume","floater","car","mat","watch","wallet","cloth","clothe","jacket","boot","sweater","trouser","hoodie","kurti","suit","handbag","jewellery","makeup","skirt","necklace","sweatshirt","shawl","saree","mobile","phone","smartphone","headset","headphone","earbud","powerbank","phone cover","mobile cover","tablet","note","speaker","earphone","smartwatch","laptop","computer","pc","camera","mouse","keyboard","cpu","ram","printer","monitor","home theatre","router","pendrive","memory card","tv","television","ac","air conditioner","fridge","refrigerator","washing machine","dishwasher","mircowave","chimney"]
+products = ["short","pant","bellie","shirt","tshirt","lower","jean","clock","decal","sticker","decor","pant","shoe","bracelet","sneaker","sandal","sunscreen","bedsheet","pillow","cushion","cup","perfume","swimwear","bra","necklace","floater","car","mat","watch","wallet","cloth","clothe","jacket","boot","sweater","trouser","hoodie","kurti","suit","handbag","jewellery","makeup","skirt","sweatshirt","shawl","saree","mobile","phone","smartphone","headset","headphone","earbud","powerbank","chain","phone cover","mobile cover","tablet","note","speaker","earphone","smartwatch","laptop","computer","pc","camera","mouse","keyboard","cpu","ram","printer","monitor","home theatre","router","pendrive","memory card","tv","television","ac","air conditioner","fridge","refrigerator","washing machine","dishwasher","mircowave","chimney"]
 ln = len(products)
 for i in range(ln):
     products.append(products[i]+"s")
