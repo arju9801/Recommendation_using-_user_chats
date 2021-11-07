5213b8b5b7f1bbf7231ad9de7e921838d4f07e1b
arju9801 <arju9801@gmail.com>
2021-11-07 11:42:01 +0530

    changes

--- fetch_products.py
+++ fetch_products.py
@@ -8,13 +8,12 @@ import os
 import nltk
 #import nltk.corpus
 #from nltk.corpus import brown
+nltk.data.path.append('./nltk/')
 from nltk.tokenize import word_tokenize
 import re
 
 
 # In[7]:
-
-nltk.data.path.append('./nltk_data/')
 
 products = ["short","pant","shirt","tshirt","lower","jean","pant","shoe","sneaker","sandal","floater","watch","wallet","jacket","boot","sweater","trouser","hoodie","kurti","suit","handbag","jewellery","makeup","skirt","necklace","sweatshirt","shawl","saree","mobile","phone","smartphone","headset","headphone","earbud","powerbank","phone cover","mobile cover","tablet","note","speaker","earphone","smartwatch","laptop","computer","pc","camera","mouse","keyboard","cpu","ram","printer","monitor","home theatre","router","pendrive","memory card","tv","television","ac","air conditioner","fridge","refrigerator","washing machine","dishwasher","mircowave","chimney"]
 ln = len(products)
